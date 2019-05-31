5ac3ae226755832915d89ca68adb5e33dbe620e2
1464770973 <1464770973@qq.com>
2019-05-31 16:31:29 +0800

    add wechatfront

---
+++ assets/js/common.js
@@ -0,0 +1,2 @@
+axios.defaults.baseURL = 'http://10.1.104.12:8087';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
---
+++ viewmodels/wechat.js
@@ -0,0 +1,48 @@
+console.log('load wechat.js');
+
+// const access_token = '17_Wi7SeShqk1mfoZhLCjtH9Xpd56-0gFoqKTw86Ni4jWzfXNUoOBco5eTRo_BohtfogQR3sqJTSQXuIhRrEG-cqM8nvyIKVr3jS9BF6cSZG-QuftGgZ5-dXUpjG7DdqGr3HNAPT1ga9_aoo8NEGOQiAAAUCE';
+const ticket = 'HoagFKDcsGMVCIY2vOjf9oNRPRdYx5XeVmEkUrh7QSILReE9ZsyflcR0Dm28X31J_QOvZxdaZT_ywF7Em9MUjQ';
+
+const appId = 'wx45c86cd90740a9bc';
+const timestamp = Date.now();
+const nonceStr = Math.random().toString(16).substr(2);
+
+const url = location.href;
+console.log(url);
+
+const originParams =
+    'jsapi_ticket=' + ticket
+    + '&noncestr=' + nonceStr
+    + '&timestamp=' + timestamp
+    + '&url=' + url;
+
+var shaObj = new jsSHA("SHA-1", "TEXT");
+shaObj.update(originParams);
+var signature = shaObj.getHash("HEX");
+
+wx.config({
+    debug: false,
+    appId: appId,
+    timestamp: timestamp,
+    nonceStr: nonceStr,
+    signature: signature,
+    jsApiList: [
+        'getLocation'
+    ]
+});
+
+wx.error(function (res) {
+    console.log('wx error');
+    console.log(res);
+});
+
+wx.ready(function () {
+    console.log('wx ready');
+});
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+    }
+})
